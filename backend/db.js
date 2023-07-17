265d7ff6d42f9582ef5317ef55d0f1c794997f7e
Célio Da Silva <celio01t@gmail.com>
2023-07-17 19:15:41 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql";
 
 export const db = mysql.createConnection({
   host: "34.29.92.71",
-  user: "celio-1270",
+  user: "root",
   password: "-+bjSsgGhgVd/]xU",
   database: "bamboo-strata-393118:us-central1:celio-1270",
 });

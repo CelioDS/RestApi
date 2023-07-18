21e33a054502ee1626365dd12e0281eb589becf3
Célio Da Silva <celio01t@gmail.com>
2023-07-17 22:35:43 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,12 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
+/*   host: "34.171.26.217",
+  user: "root",
+  password: "n#$|,CSKkO$uJ6(*",
+  database: "empresas",
+  projectId: "bamboo-strata-393118", */
+
   host: "sql.freedb.tech",
   user: "freedb_bussinessDB",
   password: "jBsxv6SVK6TY%%v",
